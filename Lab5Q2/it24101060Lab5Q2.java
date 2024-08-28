971ded8c719ea0afa01d650b1185c5ebd2babc77
it24101060 <it24101060@my.sliit.lk>
2024-08-28 06:17:45 +0530

    it24101060Lab5Q2

---
+++ Lab5Q2/it24101060Lab5Q2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class it24101060Lab5Q2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of new members introduced: ");
+        int newMembers = scanner.nextInt();
+
+            if (newMembers < 0) {
+            System.out.println("Invalid number! The number of new members should be greater than or equal to 0.");
+        } else {
+            String prize;
+            switch (newMembers) {
+                case 0:
+                    prize = "No Prize";
+                    break;
+                case 1:
+                    prize = "Pen";
+                    break;
+                case 2:
+                    prize = "Umbrella";
+                    break;
+                case 3:
+                    prize = "Bag";
+                    break;
+                case 4:
+                    prize = "Travelling Chair";
+                    break;
+                default:
+                    prize = "Headphone";
+                    break;
+            }
+
+             System.out.println("The prize you are entitled to is: " + prize);
+        }
+    }
+}
